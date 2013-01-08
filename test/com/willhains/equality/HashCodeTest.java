3ad06489e41f9ace021e91c317a7c3746f751b9b
Will Hains <github@willhains.com>
2013-01-09 05:51:13 +0900

    Refactored HashCodeTest to reduce copy-and-paste code.

--- test/com/willhains/equality/HashCodeTest.java
+++ test/com/willhains/equality/HashCodeTest.java
@@ -11,35 +11,25 @@ public class HashCodeTest
 	@Test
 	public void testHash()
 	{
-		assertTrue(0 != HashCode.compute().with("Hello world").hashCode());
-		assertTrue(0 != HashCode.computeForLargeSet().with("Hello world").hashCode());
+		_testHash(HashCode.compute());
+		_testHash(HashCode.computeForLargeSet());
+	}
+	
+	private void _testHash(final HashCode hash)
+	{
+		assertTrue(0 != hash.with("Hello world").hashCode());
 	}
 	
 	@Test
 	public void testComplexHash()
 	{
-		HashCode.compute()
-			.with(false)
-			.with((byte)117)
-			.with('g')
-			.with((short)8465)
-			.with(516572415)
-			.with(46131344334121654L)
-			.with(156453421351543454.846754348654f)
-			.with(0.0000000000000000000000000000000000000000001d)
-			.with("Leroy Jenkins!")
-			.with(new boolean[] {false, false, true})
-			.with(new byte[] {1, 2, 3})
-			.with(new char[] {'1', '2', '3'})
-			.with(new short[] {9, 9, 9})
-			.with(new int[] {5235243, 154653, 123})
-			.with(new long[] {452345234L, 63454L, 12312312L, 6456546564565L})
-			.with(new float[] {1.0f, 0.1f, 0.01f, 0.001f, 0.0001f})
-			.with(new double[] {Math.PI, Math.E, 0.99999999999999999999999999999999999999d})
-			.with(new String[] {"NCC-1701D", "NCC-1664", "NCC-2893"})
-			.hashCode(); // should throw no exceptions
-		
-		HashCode.computeForLargeSet()
+		_testComplexHash(HashCode.compute());
+		_testComplexHash(HashCode.computeForLargeSet());
+	}
+	
+	private void _testComplexHash(final HashCode hash)
+	{
+		hash
 			.with(false)
 			.with((byte)117)
 			.with('g')
@@ -64,30 +54,40 @@ public class HashCodeTest
 	@Test
 	public void testDifferent()
 	{
-		assertTrue(HashCode.compute()
-			.with("Hello").hashCode() != HashCode.compute().with("Goodbye").hashCode());
-		assertTrue(HashCode.computeForLargeSet()
-			.with("Hello").hashCode() != HashCode.compute().with("Goodbye").hashCode());
+		_testDifferent(HashCode.compute(), HashCode.compute());
+		_testDifferent(HashCode.computeForLargeSet(), HashCode.computeForLargeSet());
+	}
+	
+	private void _testDifferent(final HashCode hash1, final HashCode hash2)
+	{
+		assertTrue(hash1.with("Hello").hashCode() != hash2.with("Goodbye").hashCode());
 	}
 	
 	@Test
 	public void repeatable()
 	{
-		final int h1 = HashCode.compute().with(true).with(234838472).with("Hello").hashCode();
-		final int h2 = HashCode.compute().with(true).with(234838472).with("Hello").hashCode();
+		_repeatable(HashCode.compute(), HashCode.compute());
+		_repeatable(HashCode.computeForLargeSet(), HashCode.computeForLargeSet());
+	}
+	
+	private void _repeatable(final HashCode hash1, final HashCode hash2)
+	{
+		final int h1 = hash1.with(true).with(234838472).with("Hello").hashCode();
+		final int h2 = hash2.with(true).with(234838472).with("Hello").hashCode();
 		assertEquals(h1, h2);
-		
-		final int h3 = HashCode.computeForLargeSet().with(true).with(234838472).with("Hello").hashCode();
-		final int h4 = HashCode.computeForLargeSet().with(true).with(234838472).with("Hello").hashCode();
-		assertEquals(h3, h4);
 	}
 	
 	@Test
 	public void nulls()
 	{
+		_nulls(HashCode.compute());
+		_nulls(HashCode.computeForLargeSet());
+	}
+	
+	private void _nulls(final HashCode hash)
+	{
 		final String nullString = null;
 		final int[] nullArray = null;
-		HashCode.compute().with(nullString).with(nullArray).hashCode(); // should throw no exceptions
-		HashCode.computeForLargeSet().with(nullString).with(nullArray).hashCode(); // should throw no exceptions
+		hash.with(nullString).with(nullArray).hashCode(); // should throw no exceptions
 	}
 }
