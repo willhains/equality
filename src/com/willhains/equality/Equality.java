2cc5bc4cf54cea636ebb0e23d4c10fd252e161e3
Will Hains <github@willhains.com>
2017-05-27 01:03:02 +0900

    Special case for single-property ("simple") classes.
    Improves performance for this case.

--- src/com/willhains/equality/Equality.java
+++ src/com/willhains/equality/Equality.java
@@ -17,15 +17,8 @@ import java.util.stream.*;
  * @param <T> your class.
  * @author willhains
  */
-public final class Equality<T>
+public abstract class Equality<T>
 {
-	private final Function<T, ?>[] _properties;
-	
-	private Equality(final Function<T, ?>[] properties)
-	{
-		_properties = properties;
-	}
-	
 	/**
 	 * Create an {@link Equality} instance for your class, by supplying an array
 	 * of functions for retrieving the values of significant instance variables.
@@ -38,16 +31,15 @@ public final class Equality<T>
 	 * {@link Object#equals} and {@link Object#hashCode} implementations.
 	 */
 	@SafeVarargs
-	public static <T> Equality<T> ofProperties(final Function<T, ?>... properties)
+	public static final <T> Equality<T> ofProperties(final Function<T, ?>... properties)
 	{
-		return new Equality<>(properties);
+		return new _TupleEquality<>(properties);
 	}
 	
 	/** @see #ofProperties(Function...) */
-	@SuppressWarnings("unchecked")
-	public static <T> Equality<T> ofProperties(final Function<T, ?> property)
+	public static final <T> Equality<T> ofProperties(final Function<T, ?> property)
 	{
-		return ofProperties(new Function[] {property});
+		return new _SimpleEquality<>(property);
 	}
 	
 	/**
@@ -61,7 +53,7 @@ public final class Equality<T>
 	 * 
 	 * @see #ofProperties(Function...)
 	 */
-	public static <T> Equality<T> reflect(final Class<T> type)
+	public static final <T> Equality<T> reflect(final Class<T> type)
 	{
 		final Function<Field, Function<T, ?>> fieldToPropertyAccessor = field -> instance ->
 		{
@@ -83,38 +75,27 @@ public final class Equality<T>
 		return ofProperties(properties);
 	}
 	
+	private Equality()
+	{
+		// private subclasses only
+	}
+	
 	/**
 	 * Compare {@code self} to {@code other} for equality.
 	 * <pre>
 	 * public boolean equals(Object other) { return EQ.compare(this, other); }
 	 * </pre>
 	 */
-	public boolean compare(final T self, final Object other)
+	public final boolean compare(final T self, final Object other)
 	{
 		if(other == self) return true;
 		if(other == null) return false;
 		if(!self.getClass().equals(other.getClass())) return false;
 		@SuppressWarnings("unchecked") final T that = (T)other;
-		for(final Function<T, ?> property : _properties)
-		{
-			final Object v1 = property.apply(self);
-			final Object v2 = property.apply(that);
-			if(v1 == v2) continue;
-			if(v1 == null || v2 == null) return false;
-			if(v1.equals(v2)) continue;
-			if(v1 instanceof boolean[] && Arrays.equals((boolean[])v1, (boolean[])v2)) continue;
-			if(v1 instanceof byte[] && Arrays.equals((byte[])v1, (byte[])v2)) continue;
-			if(v1 instanceof short[] && Arrays.equals((short[])v1, (short[])v2)) continue;
-			if(v1 instanceof char[] && Arrays.equals((char[])v1, (char[])v2)) continue;
-			if(v1 instanceof int[] && Arrays.equals((int[])v1, (int[])v2)) continue;
-			if(v1 instanceof long[] && Arrays.equals((long[])v1, (long[])v2)) continue;
-			if(v1 instanceof float[] && Arrays.equals((float[])v1, (float[])v2)) continue;
-			if(v1 instanceof double[] && Arrays.equals((double[])v1, (double[])v2)) continue;
-			if(v1 instanceof Object[] && Arrays.deepEquals((Object[])v1, (Object[])v2)) continue;
-			return false;
-		}
-		return true;
+		return compareTyped(self, that);
 	}
+	
+	protected abstract boolean compareTyped(T self, T that);
 	
 	/**
 	 * Compute a hash code for {@code self}.
@@ -122,62 +103,17 @@ public final class Equality<T>
 	 * public int hashCode() { return EQ.hash(this); }
 	 * </pre>
 	 */
-	public int hash(final T self)
-	{
-		// Modified form of Joshua Bloch's hash algorithm
-		int hash = 17;
-		hash += 13 * self.getClass().hashCode();
-		for(final Function<T, ?> property : _properties)
-		{
-			final Object value = property.apply(self);
-			final int propertyHash;
-			if(value == null) propertyHash = 7;
-			else if(value.getClass().isArray())
-			{
-				if(value instanceof Object[]) propertyHash = Arrays.deepHashCode((Object[])value);
-				else if(value instanceof int[]) propertyHash = Arrays.hashCode((int[])value);
-				else if(value instanceof long[]) propertyHash = Arrays.hashCode((long[])value);
-				else if(value instanceof double[]) propertyHash = Arrays.hashCode((double[])value);
-				else if(value instanceof float[]) propertyHash = Arrays.hashCode((float[])value);
-				else if(value instanceof char[]) propertyHash = Arrays.hashCode((char[])value);
-				else if(value instanceof byte[]) propertyHash = Arrays.hashCode((byte[])value);
-				else if(value instanceof short[]) propertyHash = Arrays.hashCode((short[])value);
-				else propertyHash = Arrays.hashCode((boolean[])value);
-			}
-			else propertyHash = value.hashCode();
-			hash = 37 * hash + propertyHash;
-		}
-		return hash;
-	}
+	public abstract int hash(T self);
 	
 	/**
-	 * Build a simple, pipe-delimited string representation for {@code self},
+	 * Build a simple, pipe-delimited string representation for
+	 * {@code self},
 	 * useful for {@link Object#toString()} implementations.
 	 * <pre>
 	 * public String toString() { return EQ.format(this); }
 	 * </pre>
 	 */
-	public String format(final T self)
-	{
-		return Stream
-			.of(_properties)
-			.map($ -> $.apply(self))
-			.map(value ->
-			{
-				if(value == null) return "null";
-				else if(value instanceof Object[]) return Arrays.toString((Object[])value);
-				else if(value instanceof int[]) return Arrays.toString((int[])value);
-				else if(value instanceof long[]) return Arrays.toString((long[])value);
-				else if(value instanceof boolean[]) return Arrays.toString((boolean[])value);
-				else if(value instanceof double[]) return Arrays.toString((double[])value);
-				else if(value instanceof float[]) return Arrays.toString((float[])value);
-				else if(value instanceof char[]) return Arrays.toString((char[])value);
-				else if(value instanceof byte[]) return Arrays.toString((byte[])value);
-				else if(value instanceof short[]) return Arrays.toString((short[])value);
-				else return String.valueOf(value);
-			})
-			.collect(joining("|"));
-	}
+	public abstract String format(T self);
 	
 	/**
 	 * Build a formatted string representation for {@code self},
@@ -185,14 +121,73 @@ public final class Equality<T>
 	 * <pre>
 	 * public String toString() { return EQ.format(this, "%s: %,d (%s)"); }
 	 * </pre>
-	 * Note: The properties of {@code self} will be applied to the format string
+	 * Note: The properties of {@code self} will be applied to the format
+	 * string
 	 * in the order they are given to {@link #ofProperties}.
 	 */
-	public String format(final T self, final String formatString)
+	public abstract String format(T self, String format);
+	
+	private static final <T> int _hash(final T self, final Function<T, ?> property)
 	{
-		return String.format(
-			formatString,
-			Stream.of(_properties).map($ -> $.apply(self)).toArray(Object[]::new));
+		final Object value = property.apply(self);
+		if(value == null) return 7;
+		if(value.getClass().isArray())
+		{
+			if(value instanceof Object[]) return Arrays.deepHashCode((Object[])value);
+			if(value instanceof int[]) return Arrays.hashCode((int[])value);
+			if(value instanceof byte[]) return Arrays.hashCode((byte[])value);
+			if(value instanceof boolean[]) return Arrays.hashCode((boolean[])value);
+			if(value instanceof long[]) return Arrays.hashCode((long[])value);
+			if(value instanceof double[]) return Arrays.hashCode((double[])value);
+			if(value instanceof float[]) return Arrays.hashCode((float[])value);
+			if(value instanceof char[]) return Arrays.hashCode((char[])value);
+			return Arrays.hashCode((short[])value);
+		}
+		return value.hashCode();
+	}
+	
+	private static final <T> boolean _compare(
+		final T self,
+		final T that,
+		final Function<T, ?> property)
+	{
+		final Object v1 = property.apply(self);
+		final Object v2 = property.apply(that);
+		if(v1 == v2) return true;
+		if(v1 == null || v2 == null) return false;
+		if(v1.equals(v2)) return true;
+		if(v1.getClass().isArray())
+		{
+			if(v1 instanceof Object[]) return Arrays.deepEquals((Object[])v1, (Object[])v2);
+			if(v1 instanceof int[]) return Arrays.equals((int[])v1, (int[])v2);
+			if(v1 instanceof byte[]) return Arrays.equals((byte[])v1, (byte[])v2);
+			if(v1 instanceof boolean[]) return Arrays.equals((boolean[])v1, (boolean[])v2);
+			if(v1 instanceof long[]) return Arrays.equals((long[])v1, (long[])v2);
+			if(v1 instanceof double[]) return Arrays.equals((double[])v1, (double[])v2);
+			if(v1 instanceof float[]) return Arrays.equals((float[])v1, (float[])v2);
+			if(v1 instanceof char[]) return Arrays.equals((char[])v1, (char[])v2);
+			return Arrays.equals((short[])v1, (short[])v2);
+		}
+		return false;
+	}
+	
+	private static final <T> String _format(final T self, final Function<T, ?> property)
+	{
+		final Object value = property.apply(self);
+		if(value == null) return "null";
+		if(value.getClass().isArray())
+		{
+			if(value instanceof Object[]) return Arrays.toString((Object[])value);
+			if(value instanceof int[]) return Arrays.toString((int[])value);
+			if(value instanceof byte[]) return Arrays.toString((byte[])value);
+			if(value instanceof boolean[]) return Arrays.toString((boolean[])value);
+			if(value instanceof long[]) return Arrays.toString((long[])value);
+			if(value instanceof double[]) return Arrays.toString((double[])value);
+			if(value instanceof float[]) return Arrays.toString((float[])value);
+			if(value instanceof char[]) return Arrays.toString((char[])value);
+			return Arrays.toString((short[])value);
+		}
+		return String.valueOf(value);
 	}
 	
 	/**
@@ -202,7 +197,7 @@ public final class Equality<T>
 	 * @see #hash(Object)
 	 */
 	@Override
-	public int hashCode()
+	public final int hashCode()
 	{
 		throw new UnsupportedOperationException("Use hash(this) to compute your hash code");
 	}
@@ -215,7 +210,7 @@ public final class Equality<T>
 	 * @see #compare(Object, Object)
 	 */
 	@Override
-	public boolean equals(final Object obj)
+	public final boolean equals(final Object obj)
 	{
 		throw new UnsupportedOperationException("Use compare(this, other) to test for equality");
 	}
@@ -230,8 +225,85 @@ public final class Equality<T>
 	 * @see #format(Object, String)
 	 */
 	@Override
-	public String toString()
+	public final String toString()
 	{
 		throw new UnsupportedOperationException("Use format(this) to build your string");
+	}
+	
+	private static final class _TupleEquality<T> extends Equality<T>
+	{
+		private final Function<T, ?>[] _properties;
+		
+		private _TupleEquality(final Function<T, ?>[] properties)
+		{
+			_properties = properties;
+		}
+		
+		@Override
+		protected boolean compareTyped(final T self, final T that)
+		{
+			return Stream.of(_properties).allMatch(prop -> _compare(self, that, prop));
+		}
+		
+		@Override
+		public int hash(final T self)
+		{
+			// Modified form of Joshua Bloch's hash algorithm
+			int hash = 17;
+			hash += self.getClass().hashCode();
+			for(final Function<T, ?> prop : _properties)
+			{
+				hash = 37 * hash + _hash(self, prop);
+			}
+			return hash;
+		}
+		
+		@Override
+		public String format(final T self)
+		{
+			return Stream.of(_properties).map(prop -> _format(self, prop)).collect(joining("|"));
+		}
+		
+		@Override
+		public String format(final T self, final String formatString)
+		{
+			return String.format(
+				formatString,
+				Stream.of(_properties).map(prop -> prop.apply(self)).toArray(Object[]::new));
+		}
+	}
+	
+	private static final class _SimpleEquality<T> extends Equality<T>
+	{
+		private final Function<T, ?> _property;
+		
+		_SimpleEquality(final Function<T, ?> property)
+		{
+			_property = property;
+		}
+		
+		@Override
+		protected boolean compareTyped(final T self, final T that)
+		{
+			return _compare(self, that, _property);
+		}
+		
+		@Override
+		public int hash(final T self)
+		{
+			return _hash(self, _property);
+		}
+		
+		@Override
+		public String format(final T self)
+		{
+			return _format(self, _property);
+		}
+		
+		@Override
+		public String format(final T self, final String format)
+		{
+			return String.format(format, _property.apply(self));
+		}
 	}
 }
